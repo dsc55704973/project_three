3a1596149983451b45cd992834843642b92c2c7e
Nathan Phillips <phillips4100@gmail.com>
2020-12-01 15:25:39 -0700

    finished adding charts and predictions to HTML.  updated top of index.html with other indexes

--- HTML/html/assets/js/PredVsActChart.js
+++ HTML/html/assets/js/PredVsActChart.js
@@ -8,28 +8,131 @@ function init() {
     'Mtrls_Mnth','Tech_Mnth','Util_Mnth'];
     values.forEach(function(name) {
             dropdown.append("option").text(name).property("value");
-       
+    sector = d3.select("#selDataset").property("value")
     });
-    console.log(values[0])
-    getplots()
+    // console.log(values[0])
+    getplots(sector)
+    getpredictions()
 }
 
 init();
 
-function getplots() {
+function getplots(sector) {
+    sector = d3.select("#selDataset").property("value")
+    console.log(sector)
+    if (sector == "CS_Mnth") {
+        Plot = "../../../../assets/CS_Mnth.png"
+      } else if (sector == "CD_Mnth") {
+        Plot = "../../../../assets/CD_Mnth.png"
+      } else if (sector == "Enrgy_Mnth") {
+        Plot = "../../../../assets/Enrgy_Mnth.png"
+      }else if (sector == "Fncl_Mnth") {
+        Plot = "../../../../assets/Fncl_Mnth.png"
+      }else if (sector == "HlthC_Mnth") {
+        Plot = "../../../../assets/HlthC_Mnth.png"
+      }else if (sector == "Indstrl_Mnth") {
+        Plot = "../../../../assets/Indstrl_Mnth.png"
+      }else if (sector == "Mtrls_Mnth") {
+        Plot = "../../../../assets/Mtrls_Mnth.png"
+      }else if (sector == "Tech_Mnth") {
+        Plot = "../../../../assets/Tech_Mnth.png"
+      }else {
+        Plot = "../../../../assets/Util_Mnth.png"
+      }
+
     var img = document.createElement("img");
-        img.src = "..\..\..\assets\Util_Mnth.png";
-        img.width="900" 
-        img.height="600"
+        img.src = Plot;
+        img.width="950" 
+        img.height="500"
     var src = document.getElementById("PvA");
-        src.appendChild(PvA);
+        src.appendChild(img);
 
     console.log(img.src)
+    
 }
+
+function getpredictions() {
+    sector = d3.select("#selDataset").property("value")
+    console.log(sector)
+    if (sector == "CS_Mnth") {
+        title = "Consumer staples"
+        text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 348.78, Actual Balance: 188.61, Nostradamus: 2368.29"
+      } else if (sector == "CD_Mnth") {
+        title = "Consumer discretionary"
+        text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 682.61, Actual Balance: 465.39, Nostradamus: 8610.93"
+      } else if (sector == "Enrgy_Mnth") {
+          title = "Energy"
+          text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 186.19, Actual Balance: 61.01, Nostradamus: 10618.13"
+      }else if (sector == "Fncl_Mnth") {
+          title = "Financial"
+          text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 128.80, Actual Balance: 97.78, Nostradamus: 8814.23"
+      }else if (sector == "HlthC_Mnth") {
+          title = "Health care"
+          text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 491.54, Actual Balance: 348.08, Nostradamus: 3223.06"
+      }else if (sector == "Indstrl_Mnth") {
+          title = "Industrials"
+          text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 209.19, Actual Balance: 259.72, Nostradamus: 7576.34"
+      }else if (sector == "Mtrls_Mnth") {
+          title = "Materials"
+          text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 296.87, Actual Balance: 230.77, Nostradamus: 8420.46"
+      }else if (sector == "Tech_Mnth") {
+          title = "Information technology"
+          text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 330.75, Actual Balance: 580.33, Nostradamus: 10857.03"
+      }else {
+          title = "Utilities"
+          text = "Predicting the returns:<br>\
+        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Predicted Balance: 369.00, Actual Balance: 188.61, Nostradamus: 2368.29"
+      }
+
+   document.getElementById("pred").innerHTML = text;
+   document.getElementById("title").innerHTML = title;
+  
+}
+
+function optionChanged(sector) {
+
+    d3.select("#pred").selectAll("p").remove();
+    d3.select("#PvA").selectAll("img").remove();
+    d3.select("#title").selectAll("h4").remove();
+    var sector = d3.select("#selDataset").property("value")
+    updatePage(sector);
+    getplots(sector);
+    getpredictions()
+    // console.log(sector)
+}
+
+// // Call updatePlotly() when a change takes place
+d3.selectAll("#selDataset").on("change", updatePage);
+
+// // This function is called when a dropdown menu item is selected
+function updatePage() {
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  var sector = dropdownMenu.property("value");
+}
+
 // C:\Users\phill\Documents\Class\homework\project_three\assets\Util_Mnth.png
-var img = document.createElement("img"); 
+// var img = document.createElement("img"); 
  
-img.src = "image.png"; 
-var src = document.getElementById("x"); 
+// img.src = "image.png"; 
+// var src = document.getElementById("x"); 
  
-src.appendChild(img); 
+// src.appendChild(img); 
--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # project_three
+
+project goal was to create a market prediction model using various collected market indexes and indicators. 
+
+Haircut index:
+	https://www.economy.com/store/pro/checkout-confirmation.aspx
+  	Index Dec1997=100, NSA
+	Downloaded .csv used excel to standardize date column and python to join to the rest of dataset
+
+	If you are looking for one more indication that consumers are spending again, consider this: Hair salons are seeing an uptick in business as people schedule more haircuts 
+John Paul Dejoria, founder of hair care company Paul Mitchell, told Business Insider that “Beauty salons are the best economic indicator. Typically, customers will visit every six weeks; in downturns, that drops to every eight weeks. When it goes up again, things are on the mend.”
+*https://www.cnbc.com/id/46796981
+
+
+Cardboard:
+	https://fred.stlouisfed.org/series/PCU32221132221102 
+Index Mar 1980=100
+	
+	The cardboard box index is considered a reliable measure of manufacturing by some investors because it may reflect aggregate business estimates of future consumer goods sales. It is estimated that close to 75–80% of all non-durable goods are shipped in corrugated cardboard containers. Therefore, the thinking goes, the greater the amount of cardboard boxes ordered, the greater the volume of production planned for goods that will be packed in boxes. Because companies need cardboard to package and ship goods, the production of cardboard boxes is thought to be a leading indicator of manufacturing activity.
+*https://www.investopedia.com/terms/c/cardboardboxindex.asp
+
+
+Beer:
+	https://fred.stlouisfed.org/series/MRTSMPCSM4453USS
+Percent Change from Preceding Period, Seasonally Adjusted
+
+
+Underwear:
+	https://fred.stlouisfed.org/series/PCU3152203152201
+	Index Dec 2003=100, Not Seasonally Adjusted
+
+	If you look at a graph of men's underwear sales over the past several decades, it's almost comically flat. Men don't run out and buy underwear because it's fashionable; they buy it when they need it, and only when they need it. Since men's underwear represents the ultimate non-luxury item, Greenspan theorized that any slight dip in sales signaled a significant drop in discretionary income [source: Kurtzleben].
+
+
+Cosmetics:
+https://www.quandl.com/data/FRED/CUUR0000SEGB02Consumer-Price-Index-for-All-Urban-Consumers-Cosmetics-perfume-bath-nail-preparations-and-implements
+
+When the economy tanked during the 2001 recession, cosmetic company Estée Lauder saw a significant boom in lipstick sales. Its former chairman, Leonard Lauder, dubbed it the "lipstick index," explaining that women seek out cheap "luxuries" when money is tight [source: Wolverson].
+Turns out that Lauder was half right. Women do splurge on cheaper treats when the economy is weak, but fashion trends are another strong influence. During the Great Recession, for example, lipstick sales fell with the rest of the economy in 2008, but nail polish sales went through the roof, up 65 percent from 2008 to 2011 [sources: Davidson, Wolverson].
+
+
+unclaimed bodies:
+https://www.namus.gov/UnclaimedPersons/Search
+
+Funeral costs can be extremely high. Hence, the indicator essentially tells that one can see the state of the economy by the number of unclaimed corpses. 
+The logic here is that due to high funeral costs that people cannot afford (especially when the economy is relatively worse off), families will not claim the bodies of the deceased so that the state can pay for their funerals instead. 
+
+The Super Bowl Indicator:
+	Nfl.com
+
+	The Super Bowl Indicator is a non-scientific stock market barometer. The premise of the Super Bowl indicator is the theory that a Super Bowl win for a team from the National Football League’s American Football Conference (AFC) foretells a decline in the stock market (a bear market) in the upcoming year. Conversely, a win for a team from the National Football Conference (NFC), as well as teams from the original National Football League (NFL)–before the merger of NFL and American Football League (AFL) in 1966–means the stock market will rise in the coming year (a bull market).
+Leonard Koppett, a sportswriter for The New York Times, first introduced the Super Bowl Indicator in 1978. Up until that point, the Super Bowl Indicator had never been wrong.
+
+
+
+*https://money.howstuffworks.com/10-oddest-indicators.htm
+*https://www.businessinsider.com/bizarre-economic-indicators-2012-8
