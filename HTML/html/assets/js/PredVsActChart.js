3bf3d4176f77522616fa011c82536b470e9dfda5
Dan Jackson <djacks11784@gmail.com>
2020-12-01 21:23:52 -0700

    Final commit
    Presentation and HTML

--- HTML/html/assets/js/PredVsActChart.js
+++ HTML/html/assets/js/PredVsActChart.js
@@ -57,47 +57,47 @@ function getpredictions() {
     if (sector == "CS_Mnth") {
         title = "Consumer staples"
         text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 348.78, Actual Balance: 188.61, Nostradamus: 2368.29"
       } else if (sector == "CD_Mnth") {
         title = "Consumer discretionary"
         text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 682.61, Actual Balance: 465.39, Nostradamus: 8610.93"
       } else if (sector == "Enrgy_Mnth") {
           title = "Energy"
           text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 186.19, Actual Balance: 61.01, Nostradamus: 10618.13"
       }else if (sector == "Fncl_Mnth") {
           title = "Financial"
           text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 128.80, Actual Balance: 97.78, Nostradamus: 8814.23"
       }else if (sector == "HlthC_Mnth") {
           title = "Health care"
           text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 491.54, Actual Balance: 348.08, Nostradamus: 3223.06"
       }else if (sector == "Indstrl_Mnth") {
           title = "Industrials"
           text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 209.19, Actual Balance: 259.72, Nostradamus: 7576.34"
       }else if (sector == "Mtrls_Mnth") {
           title = "Materials"
           text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 296.87, Actual Balance: 230.77, Nostradamus: 8420.46"
       }else if (sector == "Tech_Mnth") {
           title = "Information technology"
           text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 330.75, Actual Balance: 580.33, Nostradamus: 10857.03"
       }else {
           title = "Utilities"
           text = "Predicting the returns:<br>\
-        Assuming a $100 investment starting 1/1/2016 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
+        Assuming a $100 investment starting 1/1/2006 - buy when the model predicts a positive return and sell when the model predicts a loss-<br>\
         Predicted Balance: 369.00, Actual Balance: 188.61, Nostradamus: 2368.29"
       }
 
